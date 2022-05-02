0d815606fbc5992d0c7ea8a1fc1afe85fc135a9d
Bruno Vezoli <brunvez@gmail.com>
2022-05-02 12:35:44 -0500

    Improve coverage

--- lib/scapa/cli.ex
+++ lib/scapa/cli.ex
@@ -7,7 +7,7 @@ defmodule Scapa.CLI do
   @doc """
   Receives a pattern for files to look into and generates versions for those
   """
-  def generate_versions(files_pattern \\ "lib/**/*.ex") do
+  def generate_versions(files_pattern) do
     files_to_versionate(files_pattern)
     |> Enum.map(&{&1, add_versions_to_file(&1)})
     |> Enum.filter(&elem(&1, 1))
