a2b63e999dadadf34128f166f7009dbb741da593
Bruno Vezoli <brunvez@gmail.com>
2022-05-05 12:56:25 -0500

    Format

--- lib/mix/tasks/scapa.gen.versions.ex
+++ lib/mix/tasks/scapa.gen.versions.ex
@@ -5,6 +5,7 @@ defmodule Mix.Tasks.Scapa.Gen.Versions do
 
   @doc false
   def run(_argv) do
-    for {path, content} <- Scapa.CLI.generate_versions(@default_file_pattern), do: File.write(path, content)
+    for {path, content} <- Scapa.CLI.generate_versions(@default_file_pattern),
+        do: File.write(path, content)
   end
 end
